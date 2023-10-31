a84a892a2c12bb7a06a0b83013b93f19197d62ab
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2023-10-31 11:39:01 +0530

    initial-commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:widget_testing/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Widget Testing',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Counter App'),
+    );
+  }
+}
---
+++ test/home_page_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:widget_testing/home_page.dart';
+
+void main() {
+  testWidgets(
+      'Given Counter is 0 When Increment Button Is Clicked Then Counter Should Be 1 ',
+      (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: MyHomePage(
+          title: 'Counter App',
+        ),
+      ),
+    );
+    final counter = find.text('0');
+    expect(counter, findsOneWidget);
+
+    final counter2 = find.text('1');
+    expect(counter2, findsNothing);
+
+    final incrementButton = find.byType(FloatingActionButton);
+    await tester.tap(incrementButton);
+
+    await tester.pump();
+
+    final counter3 = find.text('1');
+    expect(counter3, findsOneWidget);
+
+    final counter4 = find.text('0');
+    expect(counter4, findsNothing);
+  });
+}
